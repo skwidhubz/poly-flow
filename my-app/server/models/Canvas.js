8b48f0366812d67d35bec6071e4ef984df9a8ce2
skwidhubz <tommaxwellparker@gmail.com>
2023-05-02 17:10:37 +1000

    for loop in sandbox inst working

---
+++ my-app/server/models/Canvas.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+const userSchema = require('./User');
+
+const canvasSchema = new  Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        params: {
+            type: String,
+            required: true,
+        },
+        // access user details to save params to
+        username: [userSchema],
+    },
+);
+
+

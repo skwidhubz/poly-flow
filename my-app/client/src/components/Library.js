bac00903a242f636d5e7d01d045bae81981683a7
skwidhubz <tommaxwellparker@gmail.com>
2023-05-05 23:18:08 +1000

    nav not working

---
+++ my-app/client/src/components/Library.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../styles/library.css'
+
+const Library = () => {
+    console.log('library page rendering');
+
+    // ?data = do nothing
+    
+    return (
+      <div className="Library">
+        <header>
+          <p>
+                libray page 
+          </p>
+        </header>
+      </div>
+    );
+  }
+
+export default Library;
--- my-app/client/src/components/Nav.js
+++ my-app/client/src/components/Nav.js
@@ -11,13 +11,13 @@ const Nav = ({ activeSection, setActiveSection }) => {
     <nav className="nav">
         
       <ul id="nav-list">
-      <li className={activeSection === 'home' ? 'home' : ''} onClick={() => navigateTo('home')}><img src={logo_image} width='20%' alt='logo with shapes text reading poly flow'/></li>
+        <li className={activeSection === 'home' ? 'home' : ''} onClick={() => navigateTo('home')}><img src={logo_image} width='20%' alt='logo with shapes text reading poly flow'/></li>
         <li className={activeSection === 'create' ? 'active' : ''} onClick={() => navigateTo('create')}>Create</li>
         <li className={activeSection === 'test-modules' ? 'test-modules' : ''} onClick={() => navigateTo('test-modules')}>TestModules</li>
         <li className={activeSection === 'save' ? 'active' : ''} onClick={() => navigateTo('save')}>Save</li>
         <li className={activeSection === 'library' ? 'active' : ''} onClick={() => navigateTo('library')}>Library</li>
-        <li className={activeSection === 'login' ? 'active' : ''} onClick={() => navigateTo('login')}>Login/Signup</li>
-        <li className={activeSection === 'why' ? 'why' : ''} onClick={() => navigateTo('why')}>Why</li>
+        <li className={activeSection === 'loginform' ? 'loginform' : ''} onClick={() => navigateTo('loginform')}>login</li>
+        <li className={activeSection === 'signupform' ? 'signupform' : ''} onClick={() => navigateTo('signupform')}>signup</li>
       </ul>
     </nav>
   );
